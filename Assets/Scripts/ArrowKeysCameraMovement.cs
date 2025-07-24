29f0fca1905eb909d7d77d10af9515adb1fa2b90
Cyprian "Francuz" Arquier <carquier@edu.cdv.pl>
2025-07-24 13:24:35 +0200

    Cameratest

--- Assets/Scripts/ArrowKeysCameraMovement.cs
+++ Assets/Scripts/ArrowKeysCameraMovement.cs
@@ -1,12 +1,16 @@
+using TMPro;
 using UnityEngine;
 
 public class ArrowKeysCameraMovement : MonoBehaviour
 {
     // Speed at which the camera moves horizontally.
     public float moveSpeed = 5f;
+    public GameObject Camera;
+    public TextMeshProUGUI Text;
 
     void Update()
     {
+        Text.text = Camera.transform.position.x.ToString();
         // Get horizontal input (arrow keys or A/D).
         float horizontal = Input.GetAxis("Horizontal");
 
