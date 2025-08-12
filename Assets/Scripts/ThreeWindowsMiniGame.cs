d4147ca9d653339cba8478b26e28c1fe76a5d470
Cyprian "Francuz" Arquier <carquier@edu.cdv.pl>
2025-08-12 14:00:02 +0200

    minigame 4 final

--- Assets/Scripts/ThreeWindowsMiniGame.cs
+++ Assets/Scripts/ThreeWindowsMiniGame.cs
@@ -9,12 +9,17 @@ public class ThreeWindowsMiniGame : MonoBehaviour
     public GameObject WinScreen;
     public TextMeshProUGUI Counter;
     public List<GameObject> Items;
-    void Enable()
+    public float dispersionValueX;
+    public float dispersionValueY;
+    private void OnEnable()
     {
         ItemsInPlace = 0;
-      Dependencies.Instance.RegisterDependency<ThreeWindowsMiniGame>(this);
+        foreach (var item in Items)
+        {
+            item.transform.SetLocalPositionAndRotation(new Vector3(Random.Range(-dispersionValueX, dispersionValueX), Random.Range(-dispersionValueY, dispersionValueY), 0), Quaternion.identity);
+        }
+        Dependencies.Instance.RegisterDependency<ThreeWindowsMiniGame>(this);
     }
-
 
 
     public void CheckWindows()
